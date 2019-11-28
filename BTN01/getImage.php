4895eb41fd0ee360224945b79ffafaa8816632db
Long <thafnhlong@gmail.com>
2019-11-28 16:31:30 +0700

    Them giao dien

--- BTN01/getImage.php
+++ BTN01/getImage.php
@@ -12,11 +12,11 @@ if (!empty($_GET['id']) && !empty($_GET['type']))
 {   
     $id = $_GET['id'];
     if ($_GET['type'] == "avatar")
-        echo getImageUser($id);
+        $data=getImageUser($id);
     else if($_GET['type'] == "coverimage")
-        echo getCoverImage($id);
+        $data=getCoverImage($id);
     else 
-        echo getImagePost($id);
+        $data=getImagePost($id);
         
     $types = array('jpeg' => "\xFF\xD8\xFF", 'gif' => 'GIF', 'png' => "\x89\x50\x4e\x47\x0d\x0a", 'bmp' => 'BM', 'psd' => '8BPS', 'swf' => 'FWS');
     
--- BTN01/header.php
+++ BTN01/header.php
@@ -1,56 +1,222 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<?php
+include 'header-first.php';
+?>
+<body>	
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://kit.fontawesome.com/1e69072511.js" crossorigin="anonymous"></script>
-    <link rel='stylesheet' href='http://fonts.googleapis.com/css?family=Roboto:300,400,500,300italic'>
-    <link rel="stylesheet" href="App/modals.css">
-    <title>BTN01-SBTC</title>
-  </head>
-  <?php //require_once 'css-header.php'; ?>
-  <body>
-    <div class="container">
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="index.php">Lập trình Web 1</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item <?php echo $CurrentPage == 'index' ? 'active' : '' ?>">
-              <a class="nav-link" href="index.php">Home</a>
-            </li>
-            <?php if ($currentUser): ?>
-            <li class="nav-item <?php echo $CurrentPage == 'create-profile' ? 'active' : '' ?>">
-              <a class="nav-link" href="create-profile.php">Cập nhật thông tin cá nhân</a>
-            </li>
-            <li class="nav-item <?php echo $CurrentPage == 'profile' ? 'active' : '' ?>">
-              <a class="nav-link" href="profile.php">Trang cá nhân</a>
-            </li>
-            <?php endif; ?>
-            <?php if (!$currentUser): ?>
-            <li class="nav-item <?php echo $CurrentPage == 'register' ? 'active' : '' ?>">
-              <a class="nav-link" href="register.php">Đăng ký</a>
-            </li>
-            <li class="nav-item <?php echo $CurrentPage == 'login' ? 'active' : '' ?>">
-              <a class="nav-link" href="login.php">Đăng nhập</a>
-            </li>
-            <li class="nav-item <?php echo $CurrentPage == 'forgot_password' ? 'active' : '' ?>">
-              <a class="nav-link" href="forgot_password.php">Quên mật khẩu</a>
-            </li>
-            <?php else: ?>
-            <li class="nav-item <?php echo $CurrentPage == 'change-password' ? 'active' : '' ?>">
-              <a class="nav-link" href="change-password.php">Đổi mật khẩu</a>
-            </li>
-            <li class="nav-item <?php echo $CurrentPage == 'logout' ? 'active' : '' ?>">
-              <a class="nav-link" href="logout.php">Đăng xuất<?php echo $currentUser ? ' (' . $currentUser['Name'] . ') ' : '' ?></a>
-            </li>
-            <?php endif; ?>
-        </div>
-      </nav>
+	<div class="wrapper">	
+		<header>
+			<div class="container">
+				<div class="header-data">
+					<div class="logo">
+						<a href="index.php" title=""><img src="images/logo.png" alt=""></a>
+					</div><!--logo end-->
+					<div class="search-bar" style="visibility: hidden;">
+						<!--
+                        <form>
+							<input type="text" name="search" placeholder="Search...">
+							<button type="submit"><i class="la la-search"></i></button>
+						</form>
+                        -->
+					</div><!--search-bar end-->
+					<div class="menu-btn">
+						<a href="#" title=""><i class="fa fa-bars"></i></a>
+					</div><!--menu-btn end-->
+					<div class="user-account">
+						<div class="user-info">
+							<img src="getImage.php?type=avatar&id=<?php echo $currentUser['ID']?>" alt="">
+							<a href="#" title=""><?php echo $currentUser['Name']?></a></a>
+							<i class="la la-sort-down"></i>
+						</div>
+						<div class="user-account-settingss" id="users">
+                            <!--
+							<h3>Online Status</h3>
+							<ul class="on-off-status">
+								<li>
+									<div class="fgt-sec">
+										<input type="radio" name="cc" id="c5">
+										<label for="c5">
+											<span></span>
+										</label>
+										<small>Online</small>
+									</div>
+								</li>
+								<li>
+									<div class="fgt-sec">
+										<input type="radio" name="cc" id="c6">
+										<label for="c6">
+											<span></span>
+										</label>
+										<small>Offline</small>
+									</div>
+								</li>
+							</ul>
+							<h3>Custom Status</h3>
+							<div class="search_form">
+								<form>
+									<input type="text" name="search">
+									<button type="submit">Ok</button>
+								</form>
+							</div>
+                            -->
+							<h3>Setting</h3>
+							<ul class="us-links">
+								<li><a href="#" title="">Update Profile</a></li>
+								<li><a href="#" title="">Change Password</a></li>
+							</ul>
+							<h3 class="tc"><a href="sign-in.html" title="">Logout</a></h3>
+						</div><!--user-account-settingss end-->
+					</div>
+                    <nav>
+						<ul>
+							<li>
+								<a href="index.php" title="">
+									<span><img src="images/icon1.png" alt=""></span>
+									Home
+								</a>
+							</li>
+                            <!--
+							<li>
+								<a href="companies.html" title="">
+									<span><img src="images/icon2.png" alt=""></span>
+									Companies
+								</a>
+								<ul>
+									<li><a href="companies.html" title="">Companies</a></li>
+									<li><a href="company-profile.html" title="">Company Profile</a></li>
+								</ul>
+							</li>
+							<li>
+								<a href="projects.html" title="">
+									<span><img src="images/icon3.png" alt=""></span>
+									Projects
+								</a>
+							</li>
+                            -->
+							<li>
+								<a href="profiles.html" title="">
+									<span><img src="images/icon4.png" alt=""></span>
+									Profiles
+								</a>
+                                <!--
+								<ul>
+									<li><a href="user-profile.html" title="">User Profile</a></li>
+									<li><a href="my-profile-feed.html" title="">my-profile-feed</a></li>
+								</ul>
+                                -->
+							</li>
+                            <!--
+							<li>                               
+								<a href="jobs.html" title="">
+									<span><img src="images/icon5.png" alt=""></span>
+									Jobs
+								</a>
+							</li>
+                            
+							<li>
+								<a href="#" title="" class="not-box-openm">
+									<span><img src="images/icon6.png" alt=""></span>
+									Messages
+								</a>
+								<div class="notification-box msg" id="message">
+									<div class="nt-title">
+										<h4>Setting</h4>
+										<a href="#" title="">Clear all</a>
+									</div>
+									<div class="nott-list">
+										<div class="notfication-details">
+							  				<div class="noty-user-img">
+							  					<img src="images/resources/ny-img1.png" alt="">
+							  				</div>
+							  				<div class="notification-info">
+							  					<h3><a href="messages.html" title="">Jassica William</a> </h3>
+							  					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do.</p>
+							  					<span>2 min ago</span>
+							  				</div>
+						  				</div>
+						  				<div class="notfication-details">
+							  				<div class="noty-user-img">
+							  					<img src="images/resources/ny-img2.png" alt="">
+							  				</div>
+							  				<div class="notification-info">
+							  					<h3><a href="messages.html" title="">Jassica William</a></h3>
+							  					<p>Lorem ipsum dolor sit amet.</p>
+							  					<span>2 min ago</span>
+							  				</div>
+						  				</div>
+						  				<div class="notfication-details">
+							  				<div class="noty-user-img">
+							  					<img src="images/resources/ny-img3.png" alt="">
+							  				</div>
+							  				<div class="notification-info">
+							  					<h3><a href="messages.html" title="">Jassica William</a></h3>
+							  					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempo incididunt ut labore et dolore magna aliqua.</p>
+							  					<span>2 min ago</span>
+							  				</div>
+						  				</div>
+						  				<div class="view-all-nots">
+						  					<a href="messages.html" title="">View All Messsages</a>
+						  				</div>
+									</div>
+								</div>
+							</li>
+							<li>
+								<a href="#" title="" class="not-box-open">
+									<span><img src="images/icon7.png" alt=""></span>
+									Notification
+								</a>
+								<div class="notification-box noti" id="notification">
+									<div class="nt-title">
+										<h4>Setting</h4>
+										<a href="#" title="">Clear all</a>
+									</div>
+									<div class="nott-list">
+										<div class="notfication-details">
+							  				<div class="noty-user-img">
+							  					<img src="images/resources/ny-img1.png" alt="">
+							  				</div>
+							  				<div class="notification-info">
+							  					<h3><a href="#" title="">Jassica William</a> Comment on your project.</h3>
+							  					<span>2 min ago</span>
+							  				</div>
+						  				</div>
+						  				<div class="notfication-details">
+							  				<div class="noty-user-img">
+							  					<img src="images/resources/ny-img2.png" alt="">
+							  				</div>
+							  				<div class="notification-info">
+							  					<h3><a href="#" title="">Jassica William</a> Comment on your project.</h3>
+							  					<span>2 min ago</span>
+							  				</div>
+						  				</div>
+						  				<div class="notfication-details">
+							  				<div class="noty-user-img">
+							  					<img src="images/resources/ny-img3.png" alt="">
+							  				</div>
+							  				<div class="notification-info">
+							  					<h3><a href="#" title="">Jassica William</a> Comment on your project.</h3>
+							  					<span>2 min ago</span>
+							  				</div>
+						  				</div>
+						  				<div class="notfication-details">
+							  				<div class="noty-user-img">
+							  					<img src="images/resources/ny-img2.png" alt="">
+							  				</div>
+							  				<div class="notification-info">
+							  					<h3><a href="#" title="">Jassica William</a> Comment on your project.</h3>
+							  					<span>2 min ago</span>
+							  				</div>
+						  				</div>
+						  				<div class="view-all-nots">
+						  					<a href="#" title="">View All Notification</a>
+						  				</div>
+									</div>
+								</div>
+							</li>
+                            -->
+						</ul>
+					</nav><!--nav end-->
+					
+				</div><!--header-data end-->
+			</div>
+		</header><!--header end-->	
+
